4e4c7aaa28436edf6d74de110830a4f56f4e86cf
Calvinwilson99 <calvinwilson2017@gmail.com>
2021-01-03 17:06:20 +0530

    added event model and views

--- registration/models.py
+++ registration/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    eventName = models.CharField(max_length=15, blank=False)
+    eventDesc = models.CharField(max_length=150, blank=False)
+
+    def __str__(self):
+        return self.eventName
+
 class Register(models.Model):
     name = models.CharField(max_length=30, blank=False)
     age = models.IntegerField(blank=False)
-    eventName = models.CharField(max_length=50, blank=False)
+    college = models.CharField(max_length=30, blank=False, default='college')
+    dept = models.CharField(max_length=15, blank=False, default='dept')
+    eventName = models.CharField(max_length=50, blank=False, default='event')
 
     def __str__(self):
         return self.name
--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Events
 
 # Create your views here.
+def homeView(request):
+    eventsList = Events.objects.all()
+    context = {}
+    context['events'] = eventsList
+    return render(request, 'home.html', context)
